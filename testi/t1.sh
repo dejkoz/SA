20fe86b085f0cf0551ad7065de8174b27d92ff0d
Dejan Zivkovic <dejan.zivkovic@student.um.si>
2026-05-01 13:25:09 +0000

    Added test script and workflows

---
+++ testi/t1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+echo "Zaganjam 1. test"
+bash ./pripravi_izvajalno_okolje.sh https://github.com/dejkoz/ORV.git
---
+++ testi/t2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Zaganjam 2. test"
+bash ./pripravi_izvajalno_okolje.sh git@github.com:dejkoz/SA.git
